fc4da60f3fc27fe74c019cd03ac77d8e0fbb9925
PIYAWAN NORPALM <63010608@KMITL.AC.TH>
2020-09-05 23:00:25 +0700

    Add project files.

---
+++ lab03.2/Source.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	for (int i = 1; i <= 10000; i++)
+	{
+		int sum = 0;
+		for (int j = 1; j < i; j++)
+		{
+			if (i % j == 0) {
+				sum = sum + j;
+			}
+		}
+		if (sum == i) {
+			printf("%d ", i);
+		}
+
+	}
+}
